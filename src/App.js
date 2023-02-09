009f650acfbf487bb44b2d892ec820bee1f708fa
songhee24 <akaratel61@gmail.com>
2023-02-09 21:29:59 +0600

    trying to get payments

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
 import './App.css';
-import {useEffect, useMemo, useState} from "react";
+import {useEffect, useState} from "react";
 import axiosInstance from "./config/axiosInstance";
 
+const TitleList = (items ) => {
+     let content = <p>Not Found!</p>
+
+    console.log(items)
+    if (items && items?.length !== 0) {
+        content = items?.map(item => <li key={item.id}>{item?.address || item?.name}</li>)
+    }
+
+    return <ul>{content}</ul>
+}
 
 function App() {
     const [branches, setBranches] = useState([])
-    const [payments, setPayment] = useState([])
+    const [payments, setPayments] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         axiosInstance.get('api/branches')
             .then(r => {
-                console.log(r)
                 setBranches(r.data)
             })
             .catch(e => console.log(e))
+            .finally(() => {
+                setLoading(false)
+            })
     }, [])
 
     useEffect(() => {
         axiosInstance.get('api/payment-methods')
-            .then(r => setPayment(r.data))
+            .then(r => setPayments(r.data))
             .catch(e => console.log(e))
+            .finally(() => {
+                setLoading(false)
+            })
     }, [])
 
-    const renderPayments = useMemo(() => {
-        let content = <p>Not Found!</p>
+    if(loading) {
+        return <p>Loading...</p>
+    }
 
-        if (branches?.length !== 0) {
-            content = branches.map(payment => <li key={payment.id}>{payment.address}</li>)
-        }
-
-        return <ul>{content}</ul>
-
-    }, [branches])
 
     return (
         <div className="App">
-            {renderPayments}
+            <TitleList items={payments} />
+            <TitleList items={branches} />
         </div>
     );
 }
