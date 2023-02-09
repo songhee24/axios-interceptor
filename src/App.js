e2d131b2bc87249d99e713004e0935987f9b5812
songhee24 <akaratel61@gmail.com>
2023-02-09 21:53:55 +0600

    default navigate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import {AppRoutes} from "./routes/AppRoutes";
+import {Navigate, useLocation} from "react-router-dom";
 
 function App() {
+    const {pathname} = useLocation()
+
+    if (pathname === '/') {
+        return <Navigate to={'/login'} replace={true}/>
+    }
+
     return (
-        <AppRoutes />
+        <AppRoutes/>
     );
 }
 
