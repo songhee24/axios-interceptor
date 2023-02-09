79b4d3c476097ad75a1dbff877bed1be7850e44a
songhee24 <akaratel61@gmail.com>
2023-02-09 21:17:34 +0600

    axiosInstance.js usage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
 import {useEffect, useMemo, useState} from "react";
-import axios from "axios";
+import axiosInstance from "./config/axiosInstance";
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiQURNSU4iLCJwaG9uZU51bWJlciI6Iig2MjkpIDU1NS0wMTI5IiwiZXhwIjoxNjc2NzMyNjU0LCJpYXQiOjE2NzU4Njg2NTQsImVtYWlsIjoiYWRtaW5AZ21haWwuY29tIn0.Sz63OlUjRwm0kzmXkd5txAEbELqZ5PtLlerejPE3DdCDUmiukyAfh-vpODwzb0hsyExLzznlvg1kCOiailZLYA'
 
 function App() {
     const [branches, setBranches] = useState([])
 
     useEffect(() => {
-        axios.get('http://159.65.142.42/api/branches', {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
+        axiosInstance.get('')
             .then(r => {
                 console.log(r)
                 setBranches(r.data)
--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -43,3 +43,5 @@ axiosInstance.interceptors.response.use(
         return Promise.reject(error)
     }
 )
+
+export default axiosInstance
