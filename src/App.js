782437d931cf5e31496f3cc8d6b8caf162887543
songhee24 <akaratel61@gmail.com>
2023-02-09 21:05:29 +0600

    usage token via swagger

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import {useEffect, useMemo, useState} from "react";
 import axios from "axios";
 
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiQURNSU4iLCJwaG9uZU51bWJlciI6Iig2MjkpIDU1NS0wMTI5IiwiZXhwIjoxNjc2NzMyNjU0LCJpYXQiOjE2NzU4Njg2NTQsImVtYWlsIjoiYWRtaW5AZ21haWwuY29tIn0.Sz63OlUjRwm0kzmXkd5txAEbELqZ5PtLlerejPE3DdCDUmiukyAfh-vpODwzb0hsyExLzznlvg1kCOiailZLYA'
+
 function App() {
     const [payments, setPayments] = useState([])
 
     useEffect(() => {
-        axios.get('http://159.65.142.42/api/branches')
+        axios.get('http://159.65.142.42/api/branches', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
             .then(r => {
                 console.log(r)
                 setPayments(r.data)
