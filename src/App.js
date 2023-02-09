a43bff989d85be7cf9c9d4eefd11692d4d172b23
songhee24 <akaratel61@gmail.com>
2023-02-09 20:53:03 +0600

    simple get request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
+import {useEffect, useMemo, useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+    const [payments, setPayments] = useState([])
+
+    useEffect(() => {
+        axios.get('http://159.65.142.42/api/payment-methods')
+            .then(r => {
+                console.log(r)
+                setPayments(r.data)
+            })
+            .catch(e => console.log(e))
+
+    }, [])
+
+    const renderPayments = useMemo(() => {
+        let content = <p>Not Found!</p>
+
+        if (payments?.length !== 0) {
+            content = payments.map(payment => <li key={payment.id}>{payment.name}</li>)
+        }
+
+        return <ul>{content}</ul>
+
+    }, [payments])
+
+    return (
+        <div className="App">
+            {renderPayments}
+        </div>
+    );
 }
 
 export default App;
