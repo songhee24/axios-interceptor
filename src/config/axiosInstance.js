3c6c56f39b308cb5a19cff2b547b793eedd3fa38
songhee24 <akaratel61@gmail.com>
2023-02-09 21:12:36 +0600

    usage token inside interceptors

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -11,3 +11,15 @@ const axiosInstance = axios.create({
     baseURL,
     headers,
 })
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const updatedConfig = { ...config }
+        // need to get token from store
+        // const token = authState.getState().auth.token
+        const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiQURNSU4iLCJwaG9uZU51bWJlciI6Iig2MjkpIDU1NS0wMTI5IiwiZXhwIjoxNjc2NzMyNjU0LCJpYXQiOjE2NzU4Njg2NTQsImVtYWlsIjoiYWRtaW5AZ21haWwuY29tIn0.Sz63OlUjRwm0kzmXkd5txAEbELqZ5PtLlerejPE3DdCDUmiukyAfh-vpODwzb0hsyExLzznlvg1kCOiailZLYA'
+        if(token) {
+            updatedConfig.headers.Authorization = `Bearer ${token}`
+        }
+    }
+)
