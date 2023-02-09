51602cfd9b6ca846c72d6b8d44dc1027f535a998
songhee24 <akaratel61@gmail.com>
2023-02-09 21:52:35 +0600

    login page rendering

--- src/App.js
+++ src/App.js
@@ -1,53 +1,9 @@
 import './App.css';
-import {useEffect, useState} from "react";
-import axiosInstance from "./config/axiosInstance";
-
-const TitleList = ({items} ) => {
-     let content = <p>Not Found!</p>
-
-    if (items && items?.length !== 0) {
-        content = items?.map(item => <li key={item.id}>{item?.address || item?.name}</li>)
-    }
-
-    return <ul>{content}</ul>
-}
+import {AppRoutes} from "./routes/AppRoutes";
 
 function App() {
-    const [branches, setBranches] = useState([])
-    const [payments, setPayments] = useState([])
-    const [loading, setLoading] = useState(false)
-
-    useEffect(() => {
-        setLoading(true)
-        axiosInstance.get('api/branches')
-            .then(r => {
-                setBranches(r.data)
-            })
-            .catch(e => console.log(e))
-            .finally(() => {
-                setLoading(false)
-            })
-    }, [])
-
-    useEffect(() => {
-        axiosInstance.get('api/payment-methods')
-            .then(r => setPayments(r.data))
-            .catch(e => console.log(e))
-            .finally(() => {
-                setLoading(false)
-            })
-    }, [])
-
-    if(loading) {
-        return <p>Loading...</p>
-    }
-
-
     return (
-        <div className="App">
-            <TitleList items={payments} />
-            <TitleList items={branches} />
-        </div>
+        <AppRoutes />
     );
 }
 
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,10 +1,19 @@
 import {Route, Routes} from "react-router-dom";
+import {PrivateRoute} from "./PrivateRoute";
+import {LoginPage} from "../containers/Login.Page";
 
 export const AppRoutes = () => {
     // get token from store or is auth boolean value
+    const isAuth = false
     return (
         <Routes>
-            <Route />
+            <Route path={'/login'} element={
+                <PrivateRoute
+                    AccessComponent={LoginPage}
+                    isAuth={!isAuth}
+                    fallbackPath={'/'}
+                />}
+            />
         </Routes>
     )
 }
