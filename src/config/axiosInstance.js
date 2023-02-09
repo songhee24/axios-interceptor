6e6cb920a8382de322661204972962b252c376fe
songhee24 <akaratel61@gmail.com>
2023-02-09 21:14:14 +0600

    if some error happen

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -20,6 +20,11 @@ axiosInstance.interceptors.request.use(
         const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiQURNSU4iLCJwaG9uZU51bWJlciI6Iig2MjkpIDU1NS0wMTI5IiwiZXhwIjoxNjc2NzMyNjU0LCJpYXQiOjE2NzU4Njg2NTQsImVtYWlsIjoiYWRtaW5AZ21haWwuY29tIn0.Sz63OlUjRwm0kzmXkd5txAEbELqZ5PtLlerejPE3DdCDUmiukyAfh-vpODwzb0hsyExLzznlvg1kCOiailZLYA'
         if(token) {
             updatedConfig.headers.Authorization = `Bearer ${token}`
+            return updatedConfig
         }
+    },
+
+    (error) => {
+        return Promise.reject(error)
     }
 )
