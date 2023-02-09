a2afd7cf8dbc19fcac15b83ef318eef3289bc893
songhee24 <akaratel61@gmail.com>
2023-02-09 21:40:51 +0600

    is auth component

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,7 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 
 export const AppRoutes = () => {
-    // get token from store
+    // get token from store or is auth boolean value
     return (
         <Routes>
             <Route />
