5c142a76bb6f4eea5893e2c8a1119e7f866fe080
songhee24 <akaratel61@gmail.com>
2023-02-09 21:15:58 +0600

    Base url inside general

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
+import {baseURL} from "../constants/general";
 
 
-const baseURL = 'http://159.65.142.42/api/branches'
 
 const headers = {
     'Content-Type': 'application/json',
